4a49b4596c67b921ad1cfbcd10d07bf457045d67
James Muranga <jmured@gmail.com>
2015-11-05 00:13:02 +0300

    added login view

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,6 +1,11 @@
 from django_webtest import WebTest
 
+
 class HomeViewTestCase(WebTest):
     def test_correct_template(self):
-        home = self.app.get('/')
+        home = self.app.get('/', user="testuser")
         self.assertTemplateUsed(home, "home.html")
+
+    def test_home_requires_login(self):
+        home = self.app.get('/')
+        self.assertEqual(302, home.status_code)
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from dashboard import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.HomeView.as_view(), name='home')
 ]
