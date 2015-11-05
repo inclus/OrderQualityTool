35acd57f60105ff7c6c64de04d0797bc997b9317
James Muranga <jmured@gmail.com>
2015-11-05 09:55:23 +0300

    Setup the admin page

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib.admin import AdminSite
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+from django.contrib.auth.models import Group, User
+from django.utils.translation import ugettext_lazy
+
+
+class QdbSite(AdminSite):
+    site_title = ugettext_lazy('Order Quality Tool admin')
+
+    site_header = ugettext_lazy('Order Quality Tool administration')
+
+    index_title = ugettext_lazy('Order Quality Tool administration')
+
+
+admin_site = QdbSite()
+admin_site.register(Group, GroupAdmin)
+admin_site.register(User, UserAdmin)
--- orderqualitytool/settings.py
+++ orderqualitytool/settings.py
@@ -7,6 +7,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
+    'flat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
