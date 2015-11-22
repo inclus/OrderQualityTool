5ea865edbba9149bca1881aa0eb0bda8a69d7bac
Muhanguzi Eva <pameva77@gmail.com>
2015-11-22 17:06:35 +0000

    updated the form to allow user to select cycle when uploading file

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,7 +1,8 @@
 import os
 
+from arrow import Arrow, now
 from django.core.exceptions import ValidationError
-from django.forms import Form, FileField
+from django.forms import Form, FileField, ChoiceField
 
 
 def validate_file_extension(value):
@@ -11,5 +12,32 @@ def validate_file_extension(value):
         raise ValidationError(u'Unsupported file extension.')
 
 
+class CustomArrow(Arrow):
+    @classmethod
+    def _get_frames(cls, name):
+        if name in cls._ATTRS:
+            return name, '{0}s'.format(name), 1
+        elif name in ['week', 'weeks']:
+            return 'week', 'weeks', 1
+        elif name in ['quarter', 'quarters']:
+            return 'quarter', 'months', 3
+        elif name in ['months=2']:
+            return 'month', 'months', 2
+        raise AttributeError()
+
+
+def format_range(start, end):
+    return "%s - %s %s" % (start.format('MMM'), end.format('MMM'), start.format('YYYY'))
+
+
+def generate_cycles(start, end):
+    return [format_range(s, e) for s, e in CustomArrow.span_range("months=2", start, end)]
+
+
+def generate_choices():
+    return [(s, s) for s in generate_cycles(now().replace(years=-2), now())]
+
+
 class FileUploadForm(Form):
     import_file = FileField(validators=[validate_file_extension])
+    cycle = ChoiceField(choices=generate_choices())
--- dashboard/tests/test_forms.py
+++ dashboard/tests/test_forms.py
@@ -1,7 +1,11 @@
-from unittest import TestCase
 import os
+from unittest import TestCase
+
+import arrow
 from django.core.files.uploadedfile import SimpleUploadedFile
-from dashboard.forms import FileUploadForm
+
+from dashboard.forms import FileUploadForm, generate_cycles
+
 
 class FileUploadFormTestCase(TestCase):
     def get_fixture_path(self, name):
@@ -11,7 +15,8 @@ class FileUploadFormTestCase(TestCase):
     def test_xls_file_is_required(self):
         wrong_file = self.get_fixture_path('wrong_file.txt')
         upload_file = open(wrong_file, 'rb')
-        post_dict = {}
+        year = arrow.now().format("YYYY")
+        post_dict = {'cycle': ('Jan - Feb %s' % year)}
         file_dict = {'import_file': SimpleUploadedFile(upload_file.name, upload_file.read())}
         form = FileUploadForm(post_dict, file_dict)
         self.assertFalse(form.is_valid())
@@ -22,7 +27,16 @@ class FileUploadFormTestCase(TestCase):
     def test_xls_file_is_valid(self):
         wrong_file = self.get_fixture_path('c.xlsx')
         upload_file = open(wrong_file, 'rb')
-        post_dict = {}
+        year = arrow.now().format("YYYY")
+        post_dict = {'cycle': ('Jan - Feb %s' % year)}
         file_dict = {'import_file': SimpleUploadedFile(upload_file.name, upload_file.read())}
         form = FileUploadForm(post_dict, file_dict)
         self.assertTrue(form.is_valid())
+
+
+class GeneratorTestCase(TestCase):
+    def test_can_generate_bi_monthly_ranges(self):
+        start = arrow.Arrow(2013, 1, 1)
+        end = arrow.Arrow(2014, 1, 1)
+        ranges = generate_cycles(start, end)
+        self.assertEqual(['Jan - Feb 2013', 'Mar - Apr 2013', 'May - Jun 2013', 'Jul - Aug 2013', 'Sep - Oct 2013', 'Nov - Dec 2013', 'Jan - Feb 2014'], ranges)
