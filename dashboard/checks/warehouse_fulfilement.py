0ce4dbb1549515f3e52c2d972dc14bd5b93b7470
Muranga James <jmured@gmail.com>
2015-12-19 11:10:59 -0800

    added more unit test for the warehouse fulfilment check to make sure it works

--- dashboard/checks/warehouse_fulfilement.py
+++ dashboard/checks/warehouse_fulfilement.py
@@ -18,13 +18,16 @@ CONSUMPTION_QUERY = "consumption_query"
 
 class WarehouseFulfilment(CycleFormulationCheck):
     test = WAREHOUSE_FULFILMENT
+    F1_QUERY = "Efavirenz (TDF/3TC/EFV)"
+    F2_QUERY = "Lamivudine (ABC/3TC) 60mg/30mg [Pack 60]"
+    F3_QUERY = "(EFV) 200mg [Pack 90]"
 
     def run(self, cycle):
         prev_cycle = get_prev_cycle(cycle)
         formulations = [
-            {NAME: F1, CONSUMPTION_QUERY: "Efavirenz (TDF/3TC/EFV)"},
-            {NAME: F2, CONSUMPTION_QUERY: "Lamivudine (ABC/3TC) 60mg/30mg [Pack 60]"},
-            {NAME: F3, CONSUMPTION_QUERY: "(EFV) 200mg [Pack 90]"}
+            {NAME: F1, CONSUMPTION_QUERY: self.F1_QUERY},
+            {NAME: F2, CONSUMPTION_QUERY: self.F2_QUERY},
+            {NAME: F3, CONSUMPTION_QUERY: self.F3_QUERY}
         ]
         for formulation in formulations:
             yes = 0
@@ -33,26 +36,21 @@ class WarehouseFulfilment(CycleFormulationCheck):
             qs = Cycle.objects.filter(cycle=cycle)
             total_count = qs.count()
             for record in qs:
-                try:
-                    next_cycle_qs = Consumption.objects.annotate(consumption=Sum(PACKS_ORDERED)).filter(facility_cycle=record, formulation__icontains=formulation[CONSUMPTION_QUERY])
-                    current_cycle_qs = Consumption.objects.annotate(consumption=Sum(QUANTITY_RECEIVED)).filter(facility_cycle__facility=record.facility, facility_cycle__cycle=prev_cycle, formulation__icontains=formulation[CONSUMPTION_QUERY])
-                    number_of_consumption_records = current_cycle_qs.count()
-                    number_of_consumption_records_next_cycle = next_cycle_qs.count()
-                    amount_received = next_cycle_qs.aggregate(sum=Sum(QUANTITY_RECEIVED)).get(SUM, 0)
-                    amount_ordered = current_cycle_qs.aggregate(sum=Sum(PACKS_ORDERED)).get(SUM, 0)
-                    result = NOT_REPORTING
-                    if number_of_consumption_records == 0 or number_of_consumption_records_next_cycle == 0:
-                        not_reporting += 1
-                    elif amount_ordered == amount_received:
-                        yes += 1
-                        result = YES
-                    else:
-                        no += 1
-                        result = NO
-                except TypeError as e:
+                current_qs = Consumption.objects.annotate(consumption=Sum(PACKS_ORDERED)).filter(facility_cycle=record, formulation__icontains=formulation[CONSUMPTION_QUERY])
+                prev_qs = Consumption.objects.annotate(consumption=Sum(QUANTITY_RECEIVED)).filter(facility_cycle__facility=record.facility, facility_cycle__cycle=prev_cycle, formulation__icontains=formulation[CONSUMPTION_QUERY])
+                number_of_consumption_records = prev_qs.count()
+                number_of_consumption_records_next_cycle = current_qs.count()
+                amount_received = current_qs.aggregate(sum=Sum(QUANTITY_RECEIVED)).get(SUM, 0)
+                amount_ordered = prev_qs.aggregate(sum=Sum(PACKS_ORDERED)).get(SUM, 0)
+                result = NOT_REPORTING
+                if number_of_consumption_records == 0 or number_of_consumption_records_next_cycle == 0:
+                    not_reporting += 1
+                elif amount_ordered == amount_received:
+                    yes += 1
+                    result = YES
+                else:
                     no += 1
                     result = NO
-                finally:
-                    self.record_result_for_facility(record, result, formulation[NAME])
+                self.record_result_for_facility(record, result, formulation[NAME])
 
             self.build_cycle_formulation_score(cycle, formulation[NAME], yes, no, not_reporting, total_count)
