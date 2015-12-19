8a76a06d80205ad7ab3878138327315ff42ff4b9
Muranga James <jmured@gmail.com>
2015-12-19 14:28:50 -0800

    adding more tests for the nnrti check

--- dashboard/checks/nnrti_checks.py
+++ dashboard/checks/nnrti_checks.py
@@ -1,6 +1,7 @@
 import operator
 
 from django.db.models import Q, F, Sum
+from django.db.models.functions import Coalesce
 
 from dashboard.checks.common import Check
 from dashboard.helpers import NNRTI_CURRENT_ADULTS, NNRTI_CURRENT_PAED, NNRTI_NEW_ADULTS, NNRTI_NEW_PAED, NOT_REPORTING, YES, NO
@@ -20,62 +21,63 @@ EXISTING = 'existing'
 
 
 class NNRTI(Check):
+    formulations = [
+        {
+            TEST: NNRTI_CURRENT_ADULTS,
+            DF2: [
+                "(EFV) 600mg [Pack 30]",
+                "(NVP) 200mg [Pack 60]",
+                "(ATV/r) 300mg/100mg [Pack 30]",
+                "(LPV/r) 200mg/50mg [Pack 120"
+            ],
+            DF1: [
+                "(AZT/3TC) 300mg/150mg [Pack 60]",
+                "(TDF/3TC) 300mg/300mg [Pack 30]",
+                "(ABC/3TC) 600mg/300mg [Pack 30]"
+            ],
+            RATIO: 1,
+            FIELDS: [
+                "pmtct_consumption",
+                "art_consumption"
+            ]},
+        {
+            TEST: NNRTI_CURRENT_PAED,
+            DF2: [
+                "(EFV) 200mg [Pack 90]",
+                "(NVP) 50mg [Pack 60]",
+                "80mg/20ml oral susp [Bottle 60ml]",
+                "(LPV/r) 100mg/25mg",
+            ],
+            DF1: [
+                "(ABC/3TC) 60mg/30mg [Pack 60]",
+                "(AZT/3TC) 60mg/30mg [Pack 60]"
+            ],
+            RATIO: 4.6,
+            FIELDS: [
+                "art_consumption"
+            ]},
+        {
+            TEST: NNRTI_NEW_ADULTS,
+            DF2: ["(EFV) 600mg [Pack 30]", "(NVP) 200mg [Pack 60]", "(ATV/r) 300mg/100mg [Pack 30]", "(LPV/r) 200mg/50mg [Pack 120]"],
+            DF1: ["(AZT/3TC) 300mg/150mg [Pack 60]", "(TDF/3TC) 300mg/300mg [Pack 30]", "(ABC/3TC) 600mg/300mg [Pack 30]"],
+            RATIO: 1,
+            FIELDS: [
+                "estimated_number_of_new_patients",
+                "estimated_number_of_new_pregnant_women"
+            ]},
+        {
+            TEST: NNRTI_NEW_PAED,
+            DF2: ["(EFV) 200mg [Pack 90]", "(NVP) 50mg [Pack 60]", "(LPV/r) 80mg/20ml oral susp [Bottle 60ml]", "(LPV/r) 100mg/25mg"],
+            DF1: ["(ABC/3TC) 60mg/30mg [Pack 60]", "(AZT/3TC) 60mg/30mg [Pack 60]"],
+            RATIO: 1,
+            FIELDS: [
+                "estimated_number_of_new_patients",
+                "estimated_number_of_new_pregnant_women"
+            ]},
+    ]
+
     def run(self, cycle):
-        formulations = [
-            {
-                TEST: NNRTI_CURRENT_ADULTS,
-                DF2: [
-                    "(EFV) 600mg [Pack 30]",
-                    "(NVP) 200mg [Pack 60]",
-                    "(ATV/r) 300mg/100mg [Pack 30]",
-                    "(LPV/r) 200mg/50mg [Pack 120"
-                ],
-                DF1: [
-                    "(AZT/3TC) 300mg/150mg [Pack 60]",
-                    "(TDF/3TC) 300mg/300mg [Pack 30]",
-                    "(ABC/3TC) 600mg/300mg [Pack 30]"
-                ],
-                RATIO: 1,
-                FIELDS: [
-                    "pmtct_consumption",
-                    "art_consumption"
-                ]},
-            {
-                TEST: NNRTI_CURRENT_PAED,
-                DF2: [
-                    "(EFV) 200mg [Pack 90]",
-                    "(NVP) 50mg [Pack 60]",
-                    "80mg/20ml oral susp [Bottle 60ml]",
-                    "(LPV/r) 100mg/25mg",
-                ],
-                DF1: [
-                    "(ABC/3TC) 60mg/30mg [Pack 60]",
-                    "(AZT/3TC) 60mg/30mg [Pack 60]"
-                ],
-                RATIO: 4.6,
-                FIELDS: [
-                    "art_consumption"
-                ]},
-            {
-                TEST: NNRTI_NEW_ADULTS,
-                DF2: ["(EFV) 600mg [Pack 30]", "(NVP) 200mg [Pack 60]", "(ATV/r) 300mg/100mg [Pack 30]", "(LPV/r) 200mg/50mg [Pack 120]"],
-                DF1: ["(AZT/3TC) 300mg/150mg [Pack 60]", "(TDF/3TC) 300mg/300mg [Pack 30]", "(ABC/3TC) 600mg/300mg [Pack 30]"],
-                RATIO: 1,
-                FIELDS: [
-                    "estimated_number_of_new_patients",
-                    "estimated_number_of_new_pregnant_women"
-                ]},
-            {
-                TEST: NNRTI_NEW_PAED,
-                DF2: ["(EFV) 200mg [Pack 90]", "(NVP) 50mg [Pack 60]", "(LPV/r) 80mg/20ml oral susp [Bottle 60ml]", "(LPV/r) 100mg/25mg"],
-                DF1: ["(ABC/3TC) 60mg/30mg [Pack 60]", "(AZT/3TC) 60mg/30mg [Pack 60]"],
-                RATIO: 1,
-                FIELDS: [
-                    "estimated_number_of_new_patients",
-                    "estimated_number_of_new_pregnant_women"
-                ]},
-        ]
-        for formulation in formulations:
+        for formulation in self.formulations:
             test = formulation[TEST]
             qs = Cycle.objects.filter(cycle=cycle)
             total_count = qs.count()
@@ -84,32 +86,19 @@ class NNRTI(Check):
             no = 0
             ratio = formulation[RATIO]
             for record in qs:
-                try:
-                    df1_filter = reduce(operator.or_, (Q(formulation__contains=item) for item in formulation[DF1]))
-                    df1_sum_fields = reduce(operator.add, (F(item) for item in formulation[FIELDS]))
-                    df2_filter = reduce(operator.or_, (Q(formulation__contains=item) for item in formulation[DF1]))
-                    df1_qs = Consumption.objects.filter(facility_cycle=record).filter(df1_filter)
-                    df2_qs = Consumption.objects.filter(facility_cycle=record).filter(df2_filter)
-                    df1_count = df1_qs.count()
-                    df2_count = df2_qs.count()
-                    sum_df1 = df1_qs.aggregate(sum=Sum(df1_sum_fields)).get("sum", 0)
-                    sum_df2 = df2_qs.aggregate(sum=Sum(df1_sum_fields)).get("sum", 0)
-                    total = sum_df1 + sum_df2
-                    result = NOT_REPORTING
-                    if df1_count == 0 or df2_count == 0:
-                        not_reporting += 1
-                    elif total == 0 or 0.7 < ((sum_df1 / ratio) / sum_df2) < 1.429:
-                        yes += 1
-                        result = YES
-                    else:
-                        no += 1
-                        result = NO
-
-                except TypeError as e:
-                    no += 1
-                    result = NO
-                finally:
-                    self.record_result_for_facility(record, result, test=test)
+                df1_filter = reduce(operator.or_, (Q(formulation__contains=item) for item in formulation[DF1]))
+                sum_fields = reduce(operator.add, (F(item) for item in formulation[FIELDS]))
+                df2_filter = reduce(operator.or_, (Q(formulation__contains=item) for item in formulation[DF2]))
+                df1_qs = Consumption.objects.filter(facility_cycle=record).filter(df1_filter)
+                df2_qs = Consumption.objects.filter(facility_cycle=record).filter(df2_filter)
+                df1_count = df1_qs.count()
+                df2_count = df2_qs.count()
+                sum_df1 = df1_qs.aggregate(sum=Coalesce(Sum(sum_fields), 0)).get("sum", 0)
+                sum_df2 = df2_qs.aggregate(sum=Coalesce(Sum(sum_fields), 0)).get("sum", 0)
+                total = sum_df1 + sum_df2
+                result = NOT_REPORTING
+                no, not_reporting, result, yes = self.compare_values(ratio, no, not_reporting, yes, result, df1_count, df2_count, sum_df1, sum_df2, total)
+                self.record_result_for_facility(record, result, test=test)
             score, _ = CycleScore.objects.get_or_create(cycle=cycle, test=test)
             yes_rate = float(yes * 100) / float(total_count)
             not_reporting_rate = float(not_reporting * 100) / float(total_count)
@@ -117,3 +106,15 @@ class NNRTI(Check):
             score.no = float(no * 100) / float(total_count)
             score.not_reporting = not_reporting_rate
             score.save()
+
+    def compare_values(self, ratio, no, not_reporting, yes, result, df1_count, df2_count, sum_df1, sum_df2, total):
+        adjusted_sum_df1 = (sum_df1 / ratio)
+        if df1_count == 0 or df2_count == 0:
+            not_reporting += 1
+        elif total == 0 or 0.7 < (adjusted_sum_df1 / sum_df2) < 1.429:
+            yes += 1
+            result = YES
+        else:
+            no += 1
+            result = NO
+        return no, not_reporting, result, yes
---
+++ dashboard/tests/test_NNRTI.py
@@ -0,0 +1,141 @@
+from arrow import now
+from django.test import TestCase
+from mock import patch, call
+from model_mommy import mommy
+
+from dashboard.checks.nnrti_checks import NNRTI
+from dashboard.helpers import YES, NOT_REPORTING, NO, NNRTI_CURRENT_PAED, NNRTI_NEW_PAED, NNRTI_NEW_ADULTS
+from dashboard.models import Cycle, Consumption, CycleScore
+from locations.models import Facility
+
+
+class NNRTITestCase(TestCase):
+    def test_not_reporting_if_df1_count_is_zero(self):
+        DF1_VALUE = 10
+        DF2_VALUE = 10
+        df1_count = 0
+        df2_count = 4
+        sum_df1 = df1_count * DF1_VALUE
+        sum_df2 = df2_count * DF2_VALUE
+        total = sum_df2 + sum_df1
+        check = NNRTI()
+        no, not_reporting, result, yes = check.compare_values(4.6, 0, 0, 0, NOT_REPORTING, df1_count, df2_count, sum_df1, sum_df2, total)
+        self.assertEqual(result, NOT_REPORTING)
+        self.assertEqual(yes, 0)
+        self.assertEqual(no, 0)
+        self.assertEqual(not_reporting, 1)
+
+    def test_not_reporting_if_df2_count_is_zero(self):
+        DF1_VALUE = 10
+        DF2_VALUE = 10
+        df1_count = 2
+        df2_count = 0
+        sum_df1 = df1_count * DF1_VALUE
+        sum_df2 = df2_count * DF2_VALUE
+        total = sum_df2 + sum_df1
+        check = NNRTI()
+        no, not_reporting, result, yes = check.compare_values(4.6, 0, 0, 0, NOT_REPORTING, df1_count, df2_count, sum_df1, sum_df2, total)
+        self.assertEqual(result, NOT_REPORTING)
+        self.assertEqual(yes, 0)
+        self.assertEqual(no, 0)
+        self.assertEqual(not_reporting, 1)
+
+    def test_fails_if_total_is_zero(self):
+        DF1_VALUE = 0
+        DF2_VALUE = 0
+        df1_count = 2
+        df2_count = 2
+        sum_df1 = df1_count * DF1_VALUE
+        sum_df2 = df2_count * DF2_VALUE
+        total = sum_df2 + sum_df1
+        check = NNRTI()
+        no, not_reporting, result, yes = check.compare_values(4.6, 0, 0, 0, NOT_REPORTING, df1_count, df2_count, sum_df1, sum_df2, total)
+        self.assertEqual(result, YES)
+        self.assertEqual(yes, 1)
+        self.assertEqual(no, 0)
+        self.assertEqual(not_reporting, 0)
+
+    def test_can_pass(self):
+        DF1_VALUE = 80
+        DF2_VALUE = 10
+        df1_count = 2
+        df2_count = 4
+        sum_df1 = df1_count * DF1_VALUE
+        sum_df2 = df2_count * DF2_VALUE
+        total = sum_df2 + sum_df1
+        check = NNRTI()
+        no, not_reporting, result, yes = check.compare_values(4.6, 0, 0, 0, NOT_REPORTING, df1_count, df2_count, sum_df1, sum_df2, total)
+        self.assertEqual(result, YES)
+
+    def test_can_fail(self):
+        DF1_VALUE = 800
+        DF2_VALUE = 10
+        df1_count = 2
+        df2_count = 4
+        sum_df1 = df1_count * DF1_VALUE
+        sum_df2 = df2_count * DF2_VALUE
+        total = sum_df2 + sum_df1
+        check = NNRTI()
+        no, not_reporting, result, yes = check.compare_values(4.6, 0, 0, 0, NOT_REPORTING, df1_count, df2_count, sum_df1, sum_df2, total)
+        self.assertEqual(result, NO)
+
+    @patch("dashboard.checks.nnrti_checks.NNRTI.record_result_for_facility")
+    @patch("dashboard.checks.nnrti_checks.NNRTI.compare_values")
+    def test_check_records_results_for_facility(self, mock_method, mock_facility_method):
+        mock_method.return_value = (0, 0, YES, 1)
+        current_cycle = "Mar - Apr %s" % now().format("YYYY")
+        facility = mommy.make(Facility)
+        current_record = mommy.make(Cycle, facility=facility, cycle=current_cycle)
+        NNRTI().run(current_cycle)
+        calls = [call(current_record, YES, test=NNRTI_CURRENT_PAED), call(current_record, YES, test=NNRTI_NEW_ADULTS), call(current_record, YES, test=NNRTI_NEW_PAED)]
+        mock_facility_method.assert_has_calls(calls)
+
+    @patch("dashboard.checks.nnrti_checks.NNRTI.compare_values")
+    def test_check_records_results_for_cycle(self, mock_method):
+        mock_method.return_value = (0, 0, YES, 1)
+        current_cycle = "Mar - Apr %s" % now().format("YYYY")
+        facility = mommy.make(Facility)
+        current_record = mommy.make(Cycle, facility=facility, cycle=current_cycle)
+        NNRTI().run(current_cycle)
+        self.assertEqual(CycleScore.objects.count(), 4)
+        self.assertEqual(CycleScore.objects.all()[0].yes, 100.0)
+        self.assertEqual(CycleScore.objects.all()[0].no, 0.0)
+        self.assertEqual(CycleScore.objects.all()[0].not_reporting, 0.0)
+
+
+class TestNNRTI_CURRENT_PAED(TestCase):
+    @patch("dashboard.checks.nnrti_checks.NNRTI.compare_values")
+    def test_check_queries_correct_data(self, mock_method):
+        current_cycle = "Mar - Apr %s" % now().format("YYYY")
+        facility = mommy.make(Facility)
+        current_record = mommy.make(Cycle, facility=facility, cycle=current_cycle)
+        DF1 = [
+            "(ABC/3TC) 60mg/30mg [Pack 60]",
+            "(AZT/3TC) 60mg/30mg [Pack 60]"
+        ]
+        DF2 = [
+            "(EFV) 200mg [Pack 90]",
+            "(NVP) 50mg [Pack 60]",
+            "80mg/20ml oral susp [Bottle 60ml]",
+            "(LPV/r) 100mg/25mg",
+        ]
+        DF1_VALUE = 10
+        DF2_VALUE = 5
+        for f in DF1:
+            mommy.make(Consumption, facility_cycle=current_record, art_consumption=DF1_VALUE, formulation=f)
+        for f in DF2:
+            mommy.make(Consumption, facility_cycle=current_record, art_consumption=DF2_VALUE, formulation=f)
+        mock_method.return_value = (0, 0, YES, 1)
+        NNRTI().run(current_cycle)
+        ratio = 4.6
+        no = 0
+        not_reporting = 0
+        yes = 0
+        df1_count = len(DF1)
+        df2_count = len(DF2)
+        sum_df1 = len(DF1) * DF1_VALUE
+        sum_df2 = len(DF2) * DF2_VALUE
+        total = sum_df2 + sum_df1
+        expected_call = call(ratio, no, not_reporting, yes, NOT_REPORTING, df1_count, df2_count, sum_df1, sum_df2, total)
+        mock_method.assert_has_calls([expected_call])
+
