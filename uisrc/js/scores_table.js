ceba7a29506b242953369e9478e1c91ac9374482
James Muranga <jmured@gmail.com>
2017-11-22 19:06:03 -0800

    making sure that the formulation has a default selected

--- uisrc/js/scores_table.js
+++ uisrc/js/scores_table.js
@@ -187,7 +187,8 @@ $(document).ready(function() {
 
     _.forEach(["#cycle_select", "#formulation_select"], function(id) {
         var firstValue = $(id + " option:first").val();
-        $(id).select2("val", firstValue);
+        $(id).val(firstValue).trigger('change');
+        console.log("--id", id, firstValue);
     });
 
     $(tableId).on("click", "td", function(e) {
