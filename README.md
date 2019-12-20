bc6bf68398cebec985ff5aa85ab356bc1a3e40ea
James Muranga <jmured@gmail.com>
2019-12-20 15:36:19 -0800

    webpack config

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Order Quality Tool and Dashboard, to connect to DHIS2 data
 ## DEPLOYMENT
 The core technologies needed to run this project are:
 
-- Python 2.7 and the project dependencies in [the requirements file](requirements/prod.txt)
+- Python 3.7 and the project dependencies in [the requirements file](requirements/prod.txt)
 - Gunicorn as an application server
 - Nginx as a web server
 - Celery as a background worker
--- webpack.config.js
+++ webpack.config.js
@@ -1,101 +1,89 @@
 //less
 //angular
 //js
-var path = require('path');
-var webpack = require('webpack')
-var CompressionPlugin = require("compression-webpack-plugin")
-var WriteFilePlugin = require('write-file-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var CompressionPlugin = require("compression-webpack-plugin");
+var WriteFilePlugin = require("write-file-webpack-plugin");
 
-var isDevServer = process.argv.find(v => v.includes('livereload'));
+var isDevServer = process.argv.find(v => v.includes("livereload"));
 
 var entryFiles = ["./uisrc/entry.js"];
 var testDefinitionEntryFiles = ["./uisrc/test-definition/entry"];
 if (isDevServer) {
-    console.log('Dev server')
-    entryFiles.push("webpack-dev-server/client?http://localhost:3030")
-    testDefinitionEntryFiles.push("webpack-dev-server/client?http://localhost:3030")
-    entryFiles.push('webpack/hot/only-dev-server')
-    testDefinitionEntryFiles.push('webpack/hot/only-dev-server')
+  console.log("Dev server");
+  entryFiles.push("webpack-dev-server/client?http://localhost:3030");
+  testDefinitionEntryFiles.push(
+    "webpack-dev-server/client?http://localhost:3030"
+  );
+  entryFiles.push("webpack/hot/only-dev-server");
+  testDefinitionEntryFiles.push("webpack/hot/only-dev-server");
 }
 
 module.exports = {
-  mode: 'production',
-   optimization: {
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              // get the name. E.g. node_modules/packageName/not/this/part.js
-              // or node_modules/packageName
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-  
-              // npm package names are URL-safe, but some servers don't like @ symbols
-              return `npm.${packageName.replace('@', '')}`;
-            },
-          },
-        },
-      }
-    },
-    devtool: 'inline-source-map',
-    entry: {
-        app: entryFiles,
-        "test.definition": testDefinitionEntryFiles,
-
+  mode: "production",
+  devtool: "inline-source-map",
+  entry: {
+    app: entryFiles,
+    "test.definition": testDefinitionEntryFiles
   },
   output: {
     path: path.join(__dirname, "dashboard/static/dist"),
-    filename: "[name].[hash:8].bundle.js",
+    filename: "[name].bundle.js",
     publicPath: "/static/dist/"
   },
   module: {
-   
-    rules: [{
-      test: /\.less$/,
-      loader: "style-loader!css-loader!less-loader"
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }, {
-      test: /\.png$/,
-      loader: "url-loader?limit=100000"
-    }, {
-      test: /\.jpg$/,
-      loader: "file-loader"
-    }, {
-      test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-      loader: 'file-loader'
-    },
-    {
-      test: /\.(html)$/,
-      loader: 'html-loader'
-    }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'eslint-loader',
-            options: {
-                // fix: true
-            }
-        }]
+    rules: [
+      {
+        test: /\.less$/,
+        loader: "style-loader!css-loader!less-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(html)$/,
+        loader: "html-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          // fix: true
+        }
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
-      'window.jQuery': 'jquery',
-      'window.$': 'jquery',
-      'jQuery': 'jquery'
+      "window.jQuery": "jquery",
+      "window.$": "jquery",
+      jQuery: "jquery"
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new CompressionPlugin(),
     new WriteFilePlugin(),
-    new webpack.HashedModuleIdsPlugin(), 
+    new webpack.HashedModuleIdsPlugin()
   ],
   resolve: {
     alias: {
-      'jquery': 'jquery/dist/jquery',
-      'datatables.net': "datatables.net/js/jquery.dataTables.js",
+      jquery: "jquery/dist/jquery",
+      "datatables.net": "datatables.net/js/jquery.dataTables.js"
     }
   }
 };
